8b64b8c1a678bdf0b0de65260fa6524f26ec47e7
Andrew Osheroff <andrewosh@gmail.com>
2023-05-23 00:47:16 +0530

    Add failing Hyperbee createReadStream test (#1)
    * Add failing hyperbee createReadStream test
    
    * Create valid range options in test
    
    * fix ranges
    
    * final tweaks
    
    ---------
    
    Co-authored-by: Mathias Buus <mathiasbuus@gmail.com>

--- test.js
+++ test.js
@@ -1,5 +1,9 @@
 const b4a = require('b4a')
 const test = require('brittle')
+const Hyperbee = require('hyperbee')
+const Hypercore = require('hypercore')
+const ram = require('random-access-memory')
+
 const IndexEncoder = require('./')
 
 test('basic', function (t) {
@@ -22,20 +26,57 @@ test('basic', function (t) {
 
   t.alike(sliceAndDecode(i, [], [], keys), data)
   t.alike(sliceAndDecode(i, [0], [0], keys), [[0, 'a'], [0, 'b'], [0, 'c']])
-  t.alike(sliceAndDecode(i, [0, 'a'], [0], keys), [[0, 'b'], [0, 'c']])
-  t.alike(sliceAndDecode(i, [0, 'a'], [0, 'c'], keys), [[0, 'b']])
+  t.alike(sliceAndDecode(i, [0, 'b'], [0], keys), [[0, 'b'], [0, 'c']])
+  t.alike(sliceAndDecodeNonInclusive(i, [0, 'a'], [0, 'c'], keys), [[0, 'b']])
   t.alike(sliceAndDecode(i, [1], [1], keys), [[1, 'a']])
   t.alike(sliceAndDecode(i, [2], [], keys), [[2, 'a'], [300, 'c'], [400, 'c']])
 })
 
-function sliceAndDecode (i, gt, lt, data) {
-  const r = i.range({ gt, lt })
+test('hyperbee bounded iteration', async function (t) {
+  const keyEncoding = new IndexEncoder([
+    IndexEncoder.UINT,
+    IndexEncoder.STRING
+  ])
+  const bee = new Hyperbee(new Hypercore(ram), {
+    keyEncoding,
+    valueEncoding: 'utf-8'
+  })
 
+  await bee.put([1, 'a'], 'a')
+  await bee.put([1, 'b'], 'b')
+  await bee.put([2, 'aa'], 'aa')
+  await bee.put([2, 'bb'], 'bb')
+  await bee.put([3, 'aaa'], 'aaa')
+  await bee.put([3, 'bbb'], 'bbb')
+
+  const expectedKeys = [[2, 'aa'], [2, 'bb']]
+
+  for await (const node of bee.createReadStream({ gt: [1], lt: [3] })) {
+    t.alike(node.key, expectedKeys.shift())
+  }
+  t.is(expectedKeys.length, 0)
+})
+
+function sliceAndDecodeNonInclusive (i, gt, lt, data) {
+  const r = i.encodeRange({ gt, lt })
   const all = []
 
   for (const key of data) {
     if (b4a.compare(r.gt, key) >= 0) continue
     if (b4a.compare(key, r.lt) >= 0) continue
+    all.push(i.decode(key))
+  }
+
+  return all
+}
+
+function sliceAndDecode (i, gte, lte, data) {
+  const r = i.encodeRange({ gte, lte })
+  const all = []
+
+  for (const key of data) {
+    if (b4a.compare(r.gte, key) > 0) continue
+    if (b4a.compare(key, r.lte) > 0) continue
     all.push(i.decode(key))
   }
 
